3c0af69bf7ac38eac7b79bc80c13a2e59ef218fd
Thorsten Ehlers <thorsten.ehlers@googlemail.com>
2025-04-21 11:42:32 +0200

    Remove attribution

--- custom_components/drooff_fireplus/const.py
+++ custom_components/drooff_fireplus/const.py
@@ -5,4 +5,3 @@ from logging import Logger, getLogger
 LOGGER: Logger = getLogger(__package__)
 
 DOMAIN = "drooff_fireplus"
-ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
--- custom_components/drooff_fireplus/entity.py
+++ custom_components/drooff_fireplus/entity.py
@@ -5,14 +5,11 @@ from __future__ import annotations
 from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
-from .const import ATTRIBUTION
 from .coordinator import FireplusDataUpdateCoordinator
 
 
 class FireplusEntity(CoordinatorEntity[FireplusDataUpdateCoordinator]):
     """FireplusEntity class."""
-
-    _attr_attribution = ATTRIBUTION
 
     def __init__(self, coordinator: FireplusDataUpdateCoordinator) -> None:
         """Initialize."""
